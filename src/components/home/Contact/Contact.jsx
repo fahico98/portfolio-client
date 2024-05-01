d4b3b6073e8f149816fc7c0ada0e850cd251ab47
Fahibram Cárcamo <fahico98@gmail.com>
2024-05-01 16:25:10 -0500

    feat(contact-section): Contact home section

--- src/components/home/Contact/Contact.jsx
+++ src/components/home/Contact/Contact.jsx
@@ -4,59 +4,39 @@ import { useScrollspy } from "@/hooks/useScrollspy.js"
 function Contact() {
   const scrollspy = new useScrollspy()
 
+  function submit(event) {
+    event.preventDefault()
+    console.log("Submit...")
+  }
+
   return (
     <div id={styles.contact}>
-      <h2 className="text-2xl mb-10" id={scrollspy.links.contact.sectionId}>
-        {scrollspy.links.contact.label}
-      </h2>
-      <p>
-        Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-        filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes
-        del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un
-        latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual.
-      </p>
-      <br />
-      <p>
-        Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-        filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes
-        del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un
-        latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual.
-      </p>
-      <br />
-      <p>
-        Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-        filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes
-        del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un
-        latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual.
-      </p>
-      <br />
-      <p>
-        Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-        filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes
-        del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un
-        latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual.
-      </p>
-      <br />
-      <p>
-        Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-        filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes
-        del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un
-        latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual.
-      </p>
-      <br />
-      <p>
-        Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-        filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes
-        del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un
-        latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual.
-      </p>
-      <br />
-      <p className="mb-10">
-        Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-        filosófica, también conocida como En los extremos del bien y del mal, se dividió en cinco libros. El Lorem Ipsum que conocemos hoy se deriva de partes
-        del primer libro Liber Primus y su discusión sobre el hedonismo, cuyas palabras habían sido alteradas, añadidas y eliminadas para convertirlas en un
-        latín sin sentido e impropio. No se sabe exactamente cuándo el texto recibió su forma tradicional actual.
-      </p>
+      <div>
+        <h2 id={scrollspy.links.contact.sectionId}>{scrollspy.links.contact.label}</h2>
+        <form onSubmit={submit}>
+          <div>
+            <label htmlFor="name" className="label-md label-headline w-fit mb-1 ml-2">
+              Nombre
+            </label>
+            <input id="name" type="text" className="input-md input-high" />
+          </div>
+          <div>
+            <label htmlFor="email" className="label-md label-headline w-fit mb-1 ml-2">
+              Correo electrónico
+            </label>
+            <input id="email" type="text" className="input-md input-high" />
+          </div>
+          <div>
+            <label htmlFor="message" className="label-md label-headline w-fit mb-1 ml-2">
+              Mensaje
+            </label>
+            <textarea id="message" className="input-md input-high resize-none" rows="5" />
+          </div>
+          <button type="submit" className="btn-md btn-high w-fit">
+            Enviar&nbsp;<i className="bi bi-send"></i>
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
