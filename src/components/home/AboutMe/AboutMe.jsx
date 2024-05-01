b44f08a7a286b14828686912ed90cec1306fb724
Fahibram Cárcamo <fahico98@gmail.com>
2024-05-01 16:23:11 -0500

    feat(about-me-section): About me home section

--- src/components/home/AboutMe/AboutMe.jsx
+++ src/components/home/AboutMe/AboutMe.jsx
@@ -9,14 +9,11 @@ function AboutMe() {
     <div id={styles.aboutMe}>
       <div>
         <h2 id={scrollspy.links.aboutMe.sectionId}>{scrollspy.links.aboutMe.label}</h2>
-        <p>
-          Se cree ampliamente que la historia de Lorem Ipsum se origina. Esta obra filosófica, también conocida como En los extremos del bien y del mal, se
-          dividió en cinco libros.
-        </p>
+        <p>Mi nombre es Fahibram Cárcamo, soy un desarrollador web full-stack con mas de 6 años de experiencia, vivo en Bogotá D.C., Colombia.</p>
         <br />
         <p>
-          Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-          filosófica. El Lorem Ipsum que conocemos hoy se deriva de partes del primer libro Liber Primus y su discusión sobre el hedonismo.
+          Soy en gran medida autodidacta por lo que, con el tiempo, he adquirido la capacidad de aprender y dominar cualquier tecnología o herramienta de forma
+          rápida y precisa. Soy proactivo, recursivo y siempre enfocado en los detalles y en los resultados.
         </p>
       </div>
       <div>
