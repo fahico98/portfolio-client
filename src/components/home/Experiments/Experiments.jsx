1bead91c490ef7cdea0024a6261e4a44357eb177
Fahibram Cárcamo <fahico98@gmail.com>
2024-05-01 16:31:03 -0500

    feat(experiments): Experiments home section Cloudinary, Coinranking and Leaflet experiments ready and working

--- src/components/home/Experiments/Experiments.jsx
+++ src/components/home/Experiments/Experiments.jsx
@@ -1,4 +1,6 @@
+import coinrankingExperimentImage from "@/assets/images/coinranking_experiment.png"
 import cloudinaryExperimentImage from "@/assets/images/cloudinary_experiment.png"
+import leafletExperimentImage from "@/assets/images/leaflet_experiment.png"
 import styles from "@/components/home/Experiments/Experiments.module.css"
 import { useScrollspy } from "@/hooks/useScrollspy.js"
 import { useNavigate } from "react-router-dom"
@@ -7,45 +9,76 @@ function Experiments() {
   const scrollspy = new useScrollspy()
   const navigate = useNavigate()
 
+  const EXPERIMENTS = [
+    {
+      title: "Editor de imágenes con Cloudinary",
+      url: "/experimento/cloudinary",
+      image: cloudinaryExperimentImage,
+      description: (
+        <>
+          Editor de imágenes basado en la API de{" "}
+          <a href="https://cloudinary.com/" target="_blank" className="link-white">
+            Cloudindary
+          </a>
+          , un software como servicio enfocado en el almacenamiento y presentación de archivos multimedia. Utiliza las{" "}
+          <a href="https://cloudinary.com/documentation/image_transformations" target="_blank" className="link-white">
+            transformaciones
+          </a>{" "}
+          de Cloudinary para editar una imagen cargada por el usuario.
+        </>
+      )
+    },
+    {
+      title: "Datos de criptomonedas con Coinranking",
+      url: "/experimento/coinranking",
+      image: coinrankingExperimentImage,
+      description: (
+        <>
+          Por medio de la conexión con la API de{" "}
+          <a href="https://coinranking.com/" target="_blank" className="link-white">
+            Coinranking
+          </a>{" "}
+          se muestra una tabla con los datos de las primeras 50 criptomonedas con la mayor capitalización de mercado del momento. Los datos de la tabla se
+          actualizan cada minuto.
+        </>
+      )
+    },
+    {
+      title: "Mapa interactivo al estilo Google Maps con Leaflet",
+      url: "/experimento/leaflet",
+      image: leafletExperimentImage,
+      description: (
+        <>
+          Mapa interactivo basado en la librería{" "}
+          <a href="https://leafletjs.com/" target="_blank" className="link-white">
+            Leaflet
+          </a>{" "}
+          (de código abierto) de Javascript, con la capacidad de cambiar entre diferentes capas de visualización de mapas y de obtener la ubicación real del
+          usuario, si este otorga el permiso correspondiente.
+        </>
+      )
+    }
+  ]
+
   return (
     <div id={styles.experiments}>
+      <h2 id={scrollspy.links.experiments.sectionId}>{scrollspy.links.experiments.label}</h2>
       <div>
-        <div>
-          <h2 id={scrollspy.links.experiments.sectionId}>{scrollspy.links.experiments.label}</h2>
-          <p>
-            Se cree ampliamente que la historia de Lorem Ipsum se origina con Cicerón en el siglo I aC y su texto De Finibus bonorum et malorum. Esta obra
-            filosófica. El Lorem Ipsum que conocemos hoy se deriva de partes del primer libro Liber Primus y su discusión sobre el hedonismo.
-          </p>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.cardBody}>
-            <h6>Editor de imágenes con Cloudinary</h6>
-            <p>
-              Este pequeño ejercicio consta de un editor de imágenes basado en la API de Cloudindary, un software como servicio enfocado en el almacenamiento y
-              precentación de archivos multimedia (fotos y videos), en este caso utilicé las transformaciones de Cloudinary para editar una imagen cargada por
-              el usuario.
-            </p>
-            <button type="button" className="btn-md btn-transparent-white" onClick={() => navigate("/experimento/cloudinary")}>
-              Explorar
-            </button>
+        {EXPERIMENTS.map((experiment, index) => (
+          <div className={styles.card} key={index}>
+            <div className={styles.cardBody}>
+              <h6>{experiment.title}</h6>
+              <p>{experiment.description}</p>
+              <button type="button" className="btn-md btn-transparent-white" onClick={() => navigate(experiment.url)}>
+                Explorar&nbsp;<i className="bi bi-search"></i>
+              </button>
+            </div>
+            <div className={styles.cardImageContainer}>
+              <img src={experiment.image} alt={`${experiment.title} image`} />
+            </div>
+            <div className={`experiment-card-gradient ${styles.cardFade}`}></div>
           </div>
-          <div className={styles.cardImageContainer}>
-            <img src={cloudinaryExperimentImage} alt="Cloudinary experiment image" />
-          </div>
-          <div className={`experiment-card-gradient ${styles.cardFade}`}></div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.cardBody}>
-            <h6 className={styles.cardTitle}>Coinranking</h6>
-            <p>Descripción...</p>
-            <button className="btn-md btn-high" onClick={() => navigate("/experimento/coinranking")}>
-              Explorar
-            </button>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <h6 className={styles.cardTitle}>One Weather Map</h6>
-        </div>
+        ))}
       </div>
     </div>
   )
