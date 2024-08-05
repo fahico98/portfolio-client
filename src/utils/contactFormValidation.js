da6329ea134d53a6519ed4d5b5c317f51ba7d1e0
Fahibram Cárcamo <fahico98@gmail.com>
2024-08-04 20:48:12 -0500

    feat(contact-form): Finished and ready to deploy.

--- src/utils/contactFormValidation.js
+++ src/utils/contactFormValidation.js
@@ -1,6 +1,6 @@
 const EMAIL_REGEX =
   /^(([^<>()[\]\\.,:\s@"]+(\.[^<>()[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const ALPHA_REGEX = /^[ \-\,\.ñÑa-zA-Záéíóú]*$/
+const ALPHA_REGEX = /^[ \-,.ñÑa-zA-Záéíóú]*$/
 
 export function validateContactForm({ name, email, message }) {
   return {
